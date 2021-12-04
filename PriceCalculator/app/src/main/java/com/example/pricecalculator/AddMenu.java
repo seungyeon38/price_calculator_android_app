0c96e6b8eb4db30bf51987862b61e1ee3673546e
seungyeon <seongyeon38@gmail.com>
2021-12-05 00:23:54 +0900

    Modify dialog width

--- PriceCalculator/app/src/main/java/com/example/pricecalculator/AddMenu.java
+++ PriceCalculator/app/src/main/java/com/example/pricecalculator/AddMenu.java
@@ -8,10 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.pricecalculator.Adapters.MenuIngredientAdapter;
@@ -74,6 +78,17 @@ public class AddMenu extends AppCompatActivity {
         }
     }
 
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        int width = (int)(getResources().getDisplayMetrics().widthPixels * 0.90);
+//        int height = (int)(getResources().getDisplayMetrics().heightPixels * 0.90);
+//
+//        getDialog().getWindow().setLayout(width, height);
+//    }
+
+
     public void setRecyclerView(ArrayList<MenuIngredient> menuIngredientArrayList){
         binding.rvMenuIngredientTable.setLayoutManager(new LinearLayoutManager(this));
 //        menuIngredientArrayList = new ArrayList<>();
@@ -125,14 +140,40 @@ public class AddMenu extends AppCompatActivity {
 
             Spinner spIngredients = (Spinner) mView.findViewById(R.id.sp_ingredients);
             EditText menuIngredientWeight = (EditText) mView.findViewById(R.id.menu_ingredient_weight);
+            TextView menuIngredientUnit = (TextView) mView.findViewById(R.id.menu_ingredient_unit);
 
             ArrayAdapter<String> ingredientAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ingredient_name_list);
 
             ingredientAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spIngredients.setAdapter(ingredientAdapter);
 
+//            int width = (int)(getResources().getDisplayMetrics().widthPixels * 0.90);
+//            int height = (int)(getResources().getDisplayMetrics().heightPixels * 0.90);
+//
+//            addMenuIngredientDialog.getWindow().setLayout(width, height);
+
+            WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+            lp.copyFrom(addMenuIngredientDialog.getWindow().getAttributes());
+            lp.width = (int)(getResources().getDisplayMetrics().widthPixels * 0.90);
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
             addMenuIngredientDialog.setContentView(mView);
             addMenuIngredientDialog.show(); // 다이얼로그 띄우기
+
+            Window window = addMenuIngredientDialog.getWindow();
+            window.setAttributes(lp);
+
+            spIngredients.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    menuIngredientUnit.setText(ingredientNameToObject(ingredient_name_list.get(position)).getIngredient_unit());
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
 
             /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
 
